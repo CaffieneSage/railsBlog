581e8d656b2791fc340edaa16e40c360bc797628
Jamie <caffeinesage44@gmail.com>
2022-06-27 16:57:04 +0100

    Navbar added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def about
+  end
+
   def show
     @article = Article.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
+  get "about", to: "articles#about"
+
   resources :articles do
     resources :comments
   end
